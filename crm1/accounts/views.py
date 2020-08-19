6005393323b0a20690d828a448e9b7342a66f921
Diego Gómez <dagomez2530@gmail.com>
2020-08-19 08:49:59 -0500

    Adding navbar

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Home Page')
+    return render(request, 'accounts/dashboard.html')
 
 def products(request):
-    return HttpResponse('Products Page')
+    return render(request, 'accounts/products.html')
 
 def customer(request):
-    return HttpResponse('Customer')
+    return render(request, 'accounts/customer.html')
